c5166d17b2f59b603c134ca1413578409390f972
sottomarino <simone.ambrosi@live.com>
2021-10-21 13:28:02 +0200

    Updates

---
+++ precache-manifest.6f69dfc5fc277458327aba68ea9b2384.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "531d8a0420f53d52e262",
+    "url": "/eth_swap-master/static/css/main.77f9db13.chunk.css"
+  },
+  {
+    "revision": "531d8a0420f53d52e262",
+    "url": "/eth_swap-master/static/js/main.531d8a04.chunk.js"
+  },
+  {
+    "revision": "26972a8501de06ea4407",
+    "url": "/eth_swap-master/static/css/1.3147785b.chunk.css"
+  },
+  {
+    "revision": "26972a8501de06ea4407",
+    "url": "/eth_swap-master/static/js/1.26972a85.chunk.js"
+  },
+  {
+    "revision": "157fc38c91b2968047bd",
+    "url": "/eth_swap-master/static/js/runtime~main.157fc38c.js"
+  },
+  {
+    "revision": "7703d34b3410dddc6ae4225b0a2e99a5",
+    "url": "/eth_swap-master/index.html"
+  }
+];
